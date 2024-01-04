be12e52a710d20f830853fd8450da35b6843ed65
Nadim Hossain <145333127+nadim45448@users.noreply.github.com>
2024-01-04 18:32:42 +0600

    updated binary tree

--- Data Structure/8.Tree/1.1binaryTree.cpp
+++ Data Structure/8.Tree/1.1binaryTree.cpp
@@ -15,15 +15,13 @@ public:
 };
 void printTree(treeNode *root, int level);
 void spaceprint(int level);
-
 void printTree(treeNode *root, int level)
 {
-    // step 1
-    if (root == NULL) // tree is null or empty
+    if (root == NULL)
     {
         return;
     }
-    if (root->leftChild == NULL && root->rightChild == NULL) // step 3. this will happen after recursive call
+    if (root->leftChild == NULL && root->rightChild == NULL)
     {
         cout << root->data << endl;
         return;
@@ -34,31 +32,22 @@ void printTree(treeNode *root, int level)
         spaceprint(level);
         cout << "Root:" << root->data << endl;
     }
-    // step2
-    // cout<<"Root:"<<root->data<<endl;
-    /*spaceprint(level);
-    cout<<"Left:";
-    printTree(root->leftChild,level+1);
-
-    spaceprint(level);
-    cout<<"Right:";
-    printTree(root->rightChild,level+1); */
     if (root->leftChild != NULL)
     {
-        spaceprint(level);
+        spaceprint(level + 1);
         cout << "Left:";
         printTree(root->leftChild, level + 1);
     }
     if (root->rightChild != NULL)
     {
-        spaceprint(level);
+        spaceprint(level + 1);
         cout << "Right:";
         printTree(root->rightChild, level + 1);
     }
 }
 void spaceprint(int level)
 {
-    for (int i = 0; i< level; i++)
+    for (int i = 0; i < level; i++)
     {
         cout << "   ";
     }
@@ -66,14 +55,12 @@ void spaceprint(int level)
 int main()
 {
     int n;
-    cout << "enter number of node:";
     cin >> n;
     treeNode *allnode[n];
     for (int i = 0; i < n; i++)
     {
         allnode[i] = new treeNode(-1);
     }
-
     for (int i = 0; i < n; i++)
     {
         int val, left, right;
@@ -81,9 +68,8 @@ int main()
         allnode[i]->data = val;
         if(left>n-1 || right>n-1)
         {
-            cout<<"Invalid index"<<endl;
+            cout<<"Invalild"<<endl;
             break;
-
         }
         if (left != -1)
         {
@@ -94,35 +80,6 @@ int main()
             allnode[i]->rightChild = allnode[right];
         }
     }
-
-    printTree(allnode[0], 0);
-
+    printTree(allnode[0],0);
     return 0;
 }
-/*
-9 (total number of node)
-0 1 2
-1 3 4
-2 5 6
-3 -1 -1
-4 -1 -1
-5 7 8
-6 -1 -1
-7 -1 -1
-8 -1 -1
-
-Root:1
-left:
-    root:1
-    left:3
-    right:4
-Right:
-    root:2
-    left:
-        root:5
-        left:7
-        right:8
-    right:6
-
-
-*/
